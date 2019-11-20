7f80e15c2ca67d9fcd12f168771b933e356b302a
alex <WFallenDown@gmail.com>
2019-11-20 13:13:50 +0800

    add Index and skip link

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "Index",
+    data(){
+        return {
+            msg:"this is index"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
