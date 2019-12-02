fc143f7d0523efa57f16de4ab40d91b53229ad6a
alex <WFallenDown@gmail.com>
2019-12-02 12:07:19 +0800

    add card and list to index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -7,29 +7,46 @@
         <p style="font-size: 20px;">Thought, story and thought</p>
       </div>
     </div>
-    <div>
+    <div class="card">
       <el-row>
-  <el-col :span="18" :offset="4">Articles I like</el-col>
-</el-row>
-<el-row><el-col :span="18" :offset="4">
-<el-row>
-  <el-col :span="3" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 1 : 0">
-    <el-card :body-style="{ padding: '0px' }">
-      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-      <div style="padding: 14px;">
-        <span>好吃的汉堡</span>
-        <div class="bottom clearfix">
-          <time class="time">{{ currentDate }}</time>
-          <el-button type="text" class="button">操作按钮</el-button>
-        </div>
-      </div>
-    </el-card>
-  </el-col>
-  </el-row>
-  </el-col>
-</el-row>
+        <el-col :span="18" :offset="4">Articles I like</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="18" :offset="4">
+          <el-row>
+            <el-col :span="3" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 1 : 0">
+              <el-card :body-style="{ padding: '0px' }">
+                <img
+                  src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                  class="image"
+                />
+                <div style="padding: 14px;">
+                  <span>好吃的汉堡</span>
+                  <div class="bottom clearfix">
+                    <time class="time">2019-11-30</time>
+                    <el-button type="text" class="button">操作按钮</el-button>
+                  </div>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
     </div>
-    <div></div>
+    <div class="list">
+      <el-row>
+        <el-col :span="8" :offset="4">
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+          </el-card>
+        </el-col>
+        <el-col :span="8" :offset="2">
+          <el-card class="box-card">
+            <div v-for="o in 4" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -69,4 +86,11 @@ export default {
   font-size: 30px;
 }
 
+.card{
+  margin-bottom: 10px;
+}
+
+.list{
+  margin-bottom: 10px;
+}
 </style>
